5c271da3ba43bbacacc30acf5c1d546a383ec0ae
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-10-02 14:20:14 -0300

    webteste

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Funcionando!!!!<br/>
+      Clarnika ON 👩👩👨
+    </div>
+  );
+}
+
+export default App;
